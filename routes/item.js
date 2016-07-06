271e27fba83ec76eeb4b8ef2c6b75e681b489dcc
abelmaniquis <abelmaniquis@gmail.com>
2016-07-06 00:48:40 +0000

    travis

--- routes/item.js
+++ routes/item.js
@@ -3,15 +3,20 @@ Contains logic for mapping endpoints to service calls. This
 separates HTTP logic from the model logic.
 */
 
+/*
+Line 15, and line 24, what are these methods?
+*/
+
 var express = require('express');
 var Item = require('../services/item');
 var router = express.Router();
 
 router.get('/items', function(req, res) {
-    Item.list(function(err, items) {
+    Item.list(function(err, items) { 
         if (err) {
             return res.status(400).json(err);
         }
+        console.log(items);
         res.json(items);
     });
 });
@@ -21,17 +26,19 @@ router.post('/items', function(req, res) {
         if (err) {
             return res.status(400).json(err);
         }
+        console.log(item);
         res.status(201).json(item);
     });
 });
 
 router.put('/items',function(req,res){
-    Item.put(req.body.name, function(err,item){
-        if(err){
-            return res.status(400).json(err);
-        }
-        res.json(item);
-    })
+    Item.put(req.body.name,function(err,item){
+       if(err){
+           return res.status(400).json(err);
+       }
+       console.log(item);
+       res.status(201).json(item);
+    });
 });
 
 router.delete('/items',function(req,res){
@@ -40,8 +47,8 @@ router.delete('/items',function(req,res){
             return res.status(400).json(err);
         }
         var i = res.id
-        items.splice(items[i],1);
-        res.json(items);
+        items.splice(items[i],1)
+        res.status(200).json(items);
     })    
 });
 
